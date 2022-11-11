4f2b94f0d757ec917c9fa39f6e0b5b895fc255f6
unknown <martynasvaznonis@gmail.com>
2022-11-11 18:59:13 +0000

    Added API for preliminary evaluation of stocks

--- client/app/views.py
+++ client/app/views.py
@@ -1,20 +1,29 @@
 from app import app
 import requests as req
+import pandas as pd
 import json
+    
+
+@app.route('/')
+def hello():
+    s = get_data_from_ticker('MSFT')
+    tickers = ['AAPL', 'MSFT', 'GOOGL']
+    df = create_dataframe(tickers)
+    print(df)
+    data = pd.read_json(req.post("http://127.0.0.1:1111", data={'data': df.to_json()}).json())
+    return f"{data.loc['AAPL']}"
+
+def create_dataframe(tickers):
+    df = pd.DataFrame([get_data_from_ticker(tick) for tick in tickers])
+    return df.set_index('ticker') 
 
 def get_data_from_ticker(tick):
     data = json.loads(req.get(
         f"https://query1.finance.yahoo.com/v11/finance/quoteSummary/{tick}?modules=defaultKeyStatistics,financialData",
         headers={'user-agent': 'curl/7.55.1'}
         ).text)['quoteSummary']['result'][0]
-    book = data['defaultKeyStatistics']['bookValue']['raw']
     eps = data['defaultKeyStatistics']['trailingEps']['raw']
+    bvps = data['defaultKeyStatistics']['bookValue']['raw']
     price = data['financialData']['currentPrice']['raw']
-    pe = price/eps
-    return pe, book
-    
-
-@app.route('/')
-def hello():
-    pe, book = get_data_from_ticker('MSFT')
-    return f"Book: {book}, PE ratio: {pe}"
+    # pe = price/eps
+    return pd.Series({'ticker': tick, 'price': price, 'eps': eps, 'bvps': bvps})
---
+++ stock_quality_API/app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+api = Api(app)
+
+class StockValuator(Resource):
+    def post(self):
+        df = pd.read_json(request.form['data'])
+        eps, bvps, price = df['eps'], df['bvps'], df['price']
+        df['score'] = np.sqrt(22.5 * eps * bvps) - price
+        return df.to_json()
+
+api.add_resource(StockValuator, '/')
